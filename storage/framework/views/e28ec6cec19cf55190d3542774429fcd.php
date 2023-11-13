c6f6eca957136a0d0553c4543761846838d469e0
anubhabnayek <137760985+anubhabnayek@users.noreply.github.com>
2023-11-14 03:03:24 +0530

    Delete storage directory

--- storage/framework/views/e28ec6cec19cf55190d3542774429fcd.php
+++
@@ -1,5 +0,0 @@
-<?php echo $__env->make('admin.layout.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('main_container'); ?>
-<?php echo $__env->make('admin.layout.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php /**PATH E:\xamp\htdocs\Laravel\Musicana _application\resources\views/admin/layout/structure.blade.php ENDPATH**/ ?>
